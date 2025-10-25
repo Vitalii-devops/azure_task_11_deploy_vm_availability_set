943e8b5c6da11a0ff0105f7ab3791413797fdbe4
Vitalii <vitalyoirshko@gmail.com>
2025-10-25 19:23:03 +0300

    fix 2

--- task.ps1
+++ task.ps1
@@ -1,3 +1,7 @@
+param(
+    [string]$SshKeyPath = "~/.ssh/id_rsa.pub"
+)
+
 $location = "uksouth"
 $resourceGroupName = "mate-azure-task-11"
 $networkSecurityGroupName = "defaultnsg"
@@ -6,7 +10,7 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_ed25519.pub" -Raw
+$sshKeyPublicKey = Get-Content $SshKeyPath -Raw
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
